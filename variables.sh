a94b8de0dbb05de73ea675d19eefe1037da23472
Bala606 <balasivadev95@gmail.com>
2026-05-02 10:29:27 +0530

    session

--- variables.sh
+++ variables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo "Enter a number:"
-read num
-
-if [ $num -gt 10 ]
-then
-  echo "Number is greater than 10"
-else
-  echo "Number is 10 or less"
-fi
+for i in 1 2 3 4 5
+do
+  echo "Number: $i"
+done
